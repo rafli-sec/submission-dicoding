93ffbfd5a3c67d04e52516b71d83da3b07ee13ff
rafli-sec <raflidiga1109@gmail.com>
2024-12-21 16:36:04 +0800

    Update loadModel.js

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -1,5 +1,5 @@
 const tf = require('@tensorflow/tfjs-node');
 async function loadModel() {
-  return tf.loadGraphModel('https://storage.googleapis.com/data-train-ml/model.json');
+  return tf.loadGraphModel('https://storage.googleapis.com/ml-bucket-storage/model.json');
 }
 module.exports = loadModel;
