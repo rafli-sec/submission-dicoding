2490e01e5ec457fcc3e6d9f019a99af516d7b402
rafli-sec <raflidiga1109@gmail.com>
2024-12-22 14:55:36 +0800

    Update loadModel.js

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -1,5 +1,5 @@
 const tf = require('@tensorflow/tfjs-node');
 async function loadModel() {
-  return tf.loadGraphModel('https://storage.googleapis.com/storage-munarah-ml/model.json');
+  return tf.loadGraphModel('https://storage.googleapis.com/fatimah-nur-storage/model.json');
 }
 module.exports = loadModel;
